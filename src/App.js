8d32892f556c65d66805680a629a6b758903b1c9
Wardha Kashif <wardhakashif570@gmail.com>
2021-01-05 08:49:07 -0800

    Forms with Formik and Yup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import FormikFormWithYep from './FormikFormwithYup';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Login</h2>
+      {/* <FormikForm /> */}
+
+      <FormikFormWithYep />
+    </div>
+  );
+}
+
+export default App;
